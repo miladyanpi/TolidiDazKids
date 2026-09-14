c3ed2a25459858608af71d9d124e1a2e69f84659
yanpimina <121519307+yanpimina@users.noreply.github.com>
2026-09-14 21:36:46 +0330

    GroupBlog

--- TolidiDaz/Api/Controllers/GroupBlogController.cs
+++ TolidiDaz/Api/Controllers/GroupBlogController.cs
@@ -1,5 +1,4 @@
-﻿using ServicesLibrary.Services.GroupBlogSrv;
-using AutoMapper;
+﻿using AutoMapper;
 using DAL.Paginagion;
 using Domain;
 using Dto.Enum;
@@ -11,6 +10,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ServicesLibrary.Services.GroupBlogSrv;
+using System.Linq.Expressions;
 
 namespace Api.Controllers
 {
@@ -100,9 +101,62 @@ namespace Api.Controllers
                                                            status: ResultMessageApi.Error,
                                                            message: ResultMessageApi.DeleteError));
         }
+
+        [HttpPost("GroupBlogs/Data")]
+        public async Task<IActionResult> GetGroupBlogs([FromBody] PaginationParams @params)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return BadRequest(new ResponseApiEntities<ResultGroupBlog>
+                                                                    (entities: new List<ResultGroupBlog>(),
+                                                                    statusCode: ResultMessageApi.ErrorCode,
+                                                                    status: ResultMessageApi.Error,
+                                                                    message: ResultMessageApi.GetError,
+                                                                    countAllRecordTable: 0
+                                                                   ));
+
+                Expression<Func<GroupBlog, bool>> predicate = x => true;
+
+                if (!string.IsNullOrWhiteSpace(@params.SearchText))
+                {
+                    var search = @params.SearchText;
+
+                    predicate = x =>
+                        (x.Title ?? "").Contains(search);
+                }
+
+                var count = await _GroupBlogService.GetCountAllAsync(predicate);
+
+                var GroupBlogs = await _GroupBlogService
+                                    .GetAllAsync(predicate, page: @params.Page, take: @params.Take);
+
+
+                var mappedGroupBlogs = _mapper.Map<ICollection<ResultGroupBlog>>(GroupBlogs);
+
+                return Ok(new ResponseApiEntities<ResultGroupBlog>
+                                                                (entities: mappedGroupBlogs,
+                                                                status: ResultMessageApi.Success,
+                                                                statusCode: ResultMessageApi.SuccessCode,
+                                                                message: ResultMessageApi.GetOk,
+                                                                countAllRecordTable: count));
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseApiEntities<ResultGroupBlog>
+                                                                    (entities: new List<ResultGroupBlog>(),
+                                                                    statusCode: ResultMessageApi.ErrorCode,
+                                                                    status: ResultMessageApi.Error,
+                                                                    message: ex.Message,
+                                                                    countAllRecordTable: 0
+                                                                   ));
+            }
+
+        }
+
         [Authorize(Roles = ConstantRoles.SuperAdminName + "," + ConstantRoles.AdminName)]
-        [HttpGet("GroupBlogs")]
-        public async Task<IActionResult> GetGroupBlogs([FromQuery] PaginationParams @params)
+        [HttpGet("GroupBlogs2")]
+        public async Task<IActionResult> GetGroupBlogs2([FromQuery] PaginationParams @params)
         {
             if (!ModelState.IsValid) return BadRequest();
 
@@ -198,6 +252,35 @@ namespace Api.Controllers
                                                             statusCode: ResultMessageApi.SuccessCode,
                                                             message: ResultMessageApi.GetOk,
                                                             countAllRecordTable: GroupBlogs.Count()));
+
+        }
+        [HttpGet("GroupBlogs/ByGuid/{guid}")]
+        public async Task<IActionResult> GetGroupBlogById([FromRoute] string guid)
+        {
+            try
+            {
+                var GroupBlog = await _GroupBlogService.FirstOrDefaultAsync(s => s.IdentityCode.ToString() == guid);
+                if (GroupBlog == null)
+                    return BadRequest(new ResponseApiEntity<UpdateGroupBlog>
+                                                                              (entity: new UpdateGroupBlog(),
+                                                                              statusCode: ResultMessageApi.ErrorCode,
+                                                                              status: ResultMessageApi.Error,
+                                                                              message: ResultMessageApi.GetError));
+                var result = _mapper.Map<UpdateGroupBlog>(GroupBlog);
+                return Ok(new ResponseApiEntity<UpdateGroupBlog>
+                                                               (entity: result,
+                                                               statusCode: ResultMessageApi.SuccessCode,
+                                                               status: ResultMessageApi.Success,
+                                                               message: ResultMessageApi.GetOk));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseApiEntity<UpdateGroupBlog>
+                                                                             (entity: new UpdateGroupBlog(),
+                                                                             statusCode: ResultMessageApi.ErrorCode,
+                                                                             status: ResultMessageApi.Error,
+                                                                             message: ex.Message));
+            }
 
         }
 
