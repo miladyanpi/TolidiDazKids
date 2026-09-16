ffb04acc37a756152dcf0442ca644154d81d557b
yanpimina <121519307+yanpimina@users.noreply.github.com>
2026-09-16 11:06:45 +0330

    SendProductMethodService

---
+++ TolidiDaz/Admin/Services/SendProductMethod/SendProductMethodService.cs
@@ -0,0 +1,192 @@
+﻿using Admin.Services;
+using Admin.Services.BaseShareService;
+using CurrieTechnologies.Razor.SweetAlert2;
+using Dto.DtoPaginagion;
+using Dto.Enum;
+using Dto.Models;
+using Dto.Models.Constant;
+using Dto.Models.DtoSendProductMethod;
+using Dto.Models.ResponseApi;
+using Microsoft.JSInterop;
+using RestSharp;
+
+namespace Admin.Services.SendProductMethod
+{
+
+    public class SendProductMethodService(
+        IRootApi<ResponseApiEntities<ResultSendProductMethod>> _RootApiResultSendProductMethods,
+        IRootApi<ResponseApiEntity<ResultSendProductMethod>> RootApiResultSendProductMethod,
+        IRootApi<ResponseApiEntity<AddSendProductMethod>> RootApiAddSendProductMethod,
+        IRootApi<ResponseApiEntity<UpdateSendProductMethod>> RootApiUpdateSendProductMethod,
+        IJSRuntime JS,
+        SweetAlertService Swal
+        ) :BaseService
+    {
+        public UpdateSendProductMethod? updateSendProductMethod { get; set; } = new();
+        public AddSendProductMethod? addSendProductMethod { get; set; } = new();
+        public ResultSendProductMethod resultSendProductMethod { get; set; } = new();
+        public List<ResultSendProductMethod>? ResultSendProductMethods= new List<ResultSendProductMethod>();
+        public string? guid { get; set; }
+        public async Task GetDataAsync(string? SearchText = "")
+        {
+            try
+            {
+                var resdata = await _RootApiResultSendProductMethods.RunMethodApi($"SendProductMethods/Data", SetPaging(SearchText), method: Method.Post);
+                if (resdata != null && resdata.Status == ResultMessageApi.Success)
+                {
+                    ResultSendProductMethods = resdata.Entities.ToList() ?? [];
+                    SetPerPage(resdata.CountAllRecordTable);
+                    NotifyStateChanged();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public async Task GetAllDataAsync(string? SearchText = "")
+        {
+            try
+            {
+                var resdata = await _RootApiResultSendProductMethods.RunMethodApi($"SendProductMethods/All", null, method: Method.Get);
+                if (resdata != null && resdata.Status == ResultMessageApi.Success)
+                {
+                    ResultSendProductMethods = resdata.Entities.ToList();
+                    NotifyStateChanged();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public async Task DeleteAsync(int ID)
+        {
+            var result1 = await Swal.FireAsync(new SweetAlertOptions
+            {
+                Title = "برای حذف مطمئن هستید؟",
+                Text = "درصورت تایید شما، رکورد انتخاب شده حذف خواهد شد",
+                ShowCancelButton = true,
+                ConfirmButtonText = "بله",
+                CancelButtonText = "خیر"
+            });
+            if (!string.IsNullOrEmpty(result1.Value))
+            {
+                try
+                {
+                    var resdata = await RootApiResultSendProductMethod.RunMethodApi($"SendProductMethods/{ID}", null, method: Method.Delete);
+                    if (resdata != null && resdata.Status == ResultMessageApi.Success)
+                    {
+                        await JS.InvokeVoidAsync(ToastConstant.FunctionJavasScriptName, resdata.Message, resdata.Status);
+                        await GetDataAsync();
+                    }
+                    else if (resdata != null && resdata.Status == ResultMessageApi.Error)
+                    {
+                        _ = await Swal.FireAsync(new SweetAlertOptions
+                        {
+                            Title = "پیام",
+                            Text = resdata.Message,
+                            Icon = resdata.Status,
+                            ShowConfirmButton = true,
+                        });
+                    }
+                    else
+                    {
+                        _ = await Swal.FireAsync(new SweetAlertOptions
+                        {
+                            Title = "پیام",
+                            Text = ResultMessageApi.ErrorDisconnectApi,
+                            Icon = ResultMessageApi.Error,
+                            ShowConfirmButton = true,
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+        public async Task AddAsync()
+        {
+            try
+            {
+                var resdata = await RootApiAddSendProductMethod.RunMethodApi("SendProductMethods", addSendProductMethod, method: Method.Post);
+                if (resdata != null && resdata.Status == ResultMessageApi.Success)
+                {
+                    await JS.InvokeVoidAsync(ToastConstant.FunctionJavasScriptName, resdata.Message, resdata.Status);
+                    addSendProductMethod = new();
+                }
+                else if (resdata != null && resdata.Status == ResultMessageApi.Error)
+                {
+                    var result2 = await Swal.FireAsync(new SweetAlertOptions
+                    {
+                        Title = "پیام",
+                        Text = resdata.Message,
+                        Icon = resdata.Status,
+                        ShowConfirmButton = true,
+                    });
+                }
+                else
+                {
+                    var result2 = await Swal.FireAsync(new SweetAlertOptions
+                    {
+                        Title = "پیام",
+                        Text = ResultMessageApi.ErrorDisconnectApi,
+                        Icon = ResultMessageApi.Error,
+                        ShowConfirmButton = true,
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public async Task UpdateAsync()
+        {
+            try
+            {
+                var resdataEdit = await RootApiUpdateSendProductMethod.RunMethodApi("SendProductMethods", updateSendProductMethod, method: Method.Patch);
+                if (resdataEdit != null && resdataEdit.Status == ResultMessageApi.Success)
+                {
+                    await JS.InvokeVoidAsync(ToastConstant.FunctionJavasScriptName, resdataEdit.Message, resdataEdit.Status);
+                }
+                else if (resdataEdit != null && resdataEdit.Status == ResultMessageApi.Error)
+                {
+                    var result2 = await Swal.FireAsync(new SweetAlertOptions
+                    {
+                        Title = "پیام",
+                        Text = resdataEdit.Message,
+                        Icon = resdataEdit.Status,
+                        ShowConfirmButton = true,
+                    });
+                }
+                else
+                {
+                    var result2 = await Swal.FireAsync(new SweetAlertOptions
+                    {
+                        Title = "پیام",
+                        Text = ResultMessageApi.ErrorDisconnectApi,
+                        Icon = ResultMessageApi.Error,
+                        ShowConfirmButton = true,
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public async Task GetUpdateDataAsync()
+        {
+            try
+            {
+                var resdataEdit = await RootApiUpdateSendProductMethod.RunMethodApi($"SendProductMethods/ByGuid/{guid}", null, method: Method.Get);
+                if (resdataEdit != null && resdataEdit.Status == ResultMessageApi.Success)
+                {
+                    updateSendProductMethod = resdataEdit.Entity;
+                    NotifyStateChanged();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
