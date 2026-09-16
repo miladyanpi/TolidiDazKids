6e19a82e9241a9f3b84e47c5602f8035c6f3bf8b
yanpimina <121519307+yanpimina@users.noreply.github.com>
2026-09-16 10:47:47 +0330

    TicketService

---
+++ TolidiDaz/Admin/Services/Ticket/TicketService.cs
@@ -0,0 +1,192 @@
+﻿using Admin.Services;
+using Admin.Services.BaseShareService;
+using CurrieTechnologies.Razor.SweetAlert2;
+using Dto.DtoPaginagion;
+using Dto.Enum;
+using Dto.Models;
+using Dto.Models.Constant;
+using Dto.Models.DtoTicket;
+using Dto.Models.ResponseApi;
+using Microsoft.JSInterop;
+using RestSharp;
+
+namespace Admin.Services.Ticket
+{
+
+    public class TicketService(
+        IRootApi<ResponseApiEntities<ResultTicket>> _RootApiResultTickets,
+        IRootApi<ResponseApiEntity<ResultTicket>> RootApiResultTicket,
+        IRootApi<ResponseApiEntity<AddTicket>> RootApiAddTicket,
+        IRootApi<ResponseApiEntity<UpdateTicket>> RootApiUpdateTicket,
+        IJSRuntime JS,
+        SweetAlertService Swal
+        ) :BaseService
+    {
+        public UpdateTicket? updateTicket { get; set; } = new();
+        public AddTicket? addTicket { get; set; } = new();
+        public ResultTicket resultTicket { get; set; } = new();
+        public List<ResultTicket>? ResultTickets= new List<ResultTicket>();
+        public string? guid { get; set; }
+        public async Task GetDataAsync(string? SearchText = "")
+        {
+            try
+            {
+                var resdata = await _RootApiResultTickets.RunMethodApi($"Tickets/Data", SetPaging(SearchText), method: Method.Post);
+                if (resdata != null && resdata.Status == ResultMessageApi.Success)
+                {
+                    ResultTickets = resdata.Entities.ToList() ?? [];
+                    SetPerPage(resdata.CountAllRecordTable);
+                    NotifyStateChanged();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public async Task GetAllDataAsync(string? SearchText = "")
+        {
+            try
+            {
+                var resdata = await _RootApiResultTickets.RunMethodApi($"Tickets/All", null, method: Method.Get);
+                if (resdata != null && resdata.Status == ResultMessageApi.Success)
+                {
+                    ResultTickets = resdata.Entities.ToList();
+                    NotifyStateChanged();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public async Task DeleteAsync(int ID)
+        {
+            var result1 = await Swal.FireAsync(new SweetAlertOptions
+            {
+                Title = "برای حذف مطمئن هستید؟",
+                Text = "درصورت تایید شما، رکورد انتخاب شده حذف خواهد شد",
+                ShowCancelButton = true,
+                ConfirmButtonText = "بله",
+                CancelButtonText = "خیر"
+            });
+            if (!string.IsNullOrEmpty(result1.Value))
+            {
+                try
+                {
+                    var resdata = await RootApiResultTicket.RunMethodApi($"Tickets/{ID}", null, method: Method.Delete);
+                    if (resdata != null && resdata.Status == ResultMessageApi.Success)
+                    {
+                        await JS.InvokeVoidAsync(ToastConstant.FunctionJavasScriptName, resdata.Message, resdata.Status);
+                        await GetDataAsync();
+                    }
+                    else if (resdata != null && resdata.Status == ResultMessageApi.Error)
+                    {
+                        _ = await Swal.FireAsync(new SweetAlertOptions
+                        {
+                            Title = "پیام",
+                            Text = resdata.Message,
+                            Icon = resdata.Status,
+                            ShowConfirmButton = true,
+                        });
+                    }
+                    else
+                    {
+                        _ = await Swal.FireAsync(new SweetAlertOptions
+                        {
+                            Title = "پیام",
+                            Text = ResultMessageApi.ErrorDisconnectApi,
+                            Icon = ResultMessageApi.Error,
+                            ShowConfirmButton = true,
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+        public async Task AddAsync()
+        {
+            try
+            {
+                var resdata = await RootApiAddTicket.RunMethodApi("Tickets", addTicket, method: Method.Post);
+                if (resdata != null && resdata.Status == ResultMessageApi.Success)
+                {
+                    await JS.InvokeVoidAsync(ToastConstant.FunctionJavasScriptName, resdata.Message, resdata.Status);
+                    addTicket = new();
+                }
+                else if (resdata != null && resdata.Status == ResultMessageApi.Error)
+                {
+                    var result2 = await Swal.FireAsync(new SweetAlertOptions
+                    {
+                        Title = "پیام",
+                        Text = resdata.Message,
+                        Icon = resdata.Status,
+                        ShowConfirmButton = true,
+                    });
+                }
+                else
+                {
+                    var result2 = await Swal.FireAsync(new SweetAlertOptions
+                    {
+                        Title = "پیام",
+                        Text = ResultMessageApi.ErrorDisconnectApi,
+                        Icon = ResultMessageApi.Error,
+                        ShowConfirmButton = true,
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public async Task UpdateAsync()
+        {
+            try
+            {
+                var resdataEdit = await RootApiUpdateTicket.RunMethodApi("Tickets", updateTicket, method: Method.Patch);
+                if (resdataEdit != null && resdataEdit.Status == ResultMessageApi.Success)
+                {
+                    await JS.InvokeVoidAsync(ToastConstant.FunctionJavasScriptName, resdataEdit.Message, resdataEdit.Status);
+                }
+                else if (resdataEdit != null && resdataEdit.Status == ResultMessageApi.Error)
+                {
+                    var result2 = await Swal.FireAsync(new SweetAlertOptions
+                    {
+                        Title = "پیام",
+                        Text = resdataEdit.Message,
+                        Icon = resdataEdit.Status,
+                        ShowConfirmButton = true,
+                    });
+                }
+                else
+                {
+                    var result2 = await Swal.FireAsync(new SweetAlertOptions
+                    {
+                        Title = "پیام",
+                        Text = ResultMessageApi.ErrorDisconnectApi,
+                        Icon = ResultMessageApi.Error,
+                        ShowConfirmButton = true,
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public async Task GetUpdateDataAsync()
+        {
+            try
+            {
+                var resdataEdit = await RootApiUpdateTicket.RunMethodApi($"Tickets/ByGuid/{guid}", null, method: Method.Get);
+                if (resdataEdit != null && resdataEdit.Status == ResultMessageApi.Success)
+                {
+                    updateTicket = resdataEdit.Entity;
+                    NotifyStateChanged();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
